ab836cae2a852416320ecf796fae32ca046532f6
Tobias Kraze <tobias@kraze.eu>
2012-05-11 15:48:22 +0200

    force users to manually require in gemspec, so we are not dependent on random load orders

--- lib/rspec_candy.rb
+++ lib/rspec_candy.rb
@@ -1,16 +1,2 @@
-require 'rspec_candy/version'
-require 'rspec_candy/switcher'
-require 'rspec_candy/helpers/disposable_copy'
-require 'rspec_candy/helpers/it_should_act_like'
-require 'rspec_candy/helpers/new_with_stubs'
-require 'rspec_candy/helpers/should_receive_and_execute'
-require 'rspec_candy/helpers/should_receive_and_return'
-require 'rspec_candy/helpers/should_receive_chain'
-require 'rspec_candy/helpers/stub_any_instance'
-require 'rspec_candy/helpers/stub_existing'
-
-if RSpecCandy::Switcher.rails_loaded?
-  require 'rspec_candy/helpers/rails/create_without_callbacks'
-  require 'rspec_candy/helpers/rails/it_should_run_callbacks'
-  require 'rspec_candy/helpers/rails/prevent_storage'
-end
+# empty, so Bundler does not require anything
+# put a "require rspec_candy/helpers" into your spec_helper
--- lib/rspec_candy/version.rb
+++ lib/rspec_candy/version.rb
@@ -1,3 +1,3 @@
 module RSpecCandy
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
